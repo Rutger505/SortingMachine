f37176b8353263d765449a3a3df69fd948659b30
Rutger Pronk <rutger.pronk11@gmail.com>
2025-11-07 15:40:06 +0100

    Paste training file from colab

--- src/training/main.py
+++ src/training/main.py
@@ -1,1 +1,41 @@
-# TODO
+import os
+from google.colab import userdata
+from roboflow import Roboflow
+import ultralytics
+from ultralytics import YOLO
+import cv2
+
+# Download dataset from Roboflow
+ROBOFLOW_API_KEY = userdata.get('ROBOFLOW_API_KEY')
+roboflow = Roboflow(api_key=ROBOFLOW_API_KEY)
+
+# Replace with your workspace and projects
+project = roboflow.workspace("sortingmachine").project(
+    "colordectection-rlg1a"
+)
+
+version = project.version(1)
+dataset = version.download("folder")
+
+# TODO improve
+# Train classification model
+model = YOLO("yolov8n-cls.pt")
+
+results = model.train(
+    data=dataset.location,
+    epochs=50,
+    imgsz=224,
+    patience=10
+)
+
+# Evaluate
+metrics = model.val()
+
+# Test on an image
+test_image = "path_to_test_image.jpg"
+predictions = model.predict(test_image)
+
+os.getcwd()
+
+# Export model to pth
+model.export()
