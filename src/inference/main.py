8587ce2283b9180de976711da47ac7e377eb0827
Rutger Pronk <rutger.pronk11@gmail.com>
2025-11-07 16:55:23 +0100

    Create a basic setup that loads a model and does a prediction

--- src/inference/main.py
+++ src/inference/main.py
@@ -1,3 +1,14 @@
 # TODO inference logic
+import os
+
+from src.inference.model import ColorDetection
+
+
+def main():
+    color_detector = ColorDetection(f"{os.getcwd()}/../../best.pt")
+
+    print(color_detector.predict(f"{os.getcwd()}/../../test.jpg"))
+
+
 if __name__ == '__main__':
-    print('PyCharm')
+    main()
--- src/inference/model.py
+++ src/inference/model.py
@@ -1,1 +1,9 @@
-# TODO
+from ultralytics import YOLO
+
+
+class ColorDetection:
+    def __init__(self, model_file: str):
+        self.model = YOLO(model_file)
+
+    def predict(self, image):
+        return self.model(image)
